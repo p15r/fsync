e4b410417d1cf9c142ec25604cfb6a0dc6bd4c1f
pat <pat@p15r.net>
2022-12-07 15:26:33 +0000

    fix: make mypy compliant

--- ftp-sync.py
+++ ftp-sync.py
@@ -327,10 +327,11 @@ def main() -> int:
         logging.info('No files found on target...')
         empty_target = True
 
+    target_lib_converted = []
     if not empty_target:
-        target_lib = _to_list(config, target_lib)
+        target_lib_converted = _to_list(config, target_lib)
 
-    add, remove = _calculate_delta(local_lib, target_lib)
+    add, remove = _calculate_delta(local_lib, target_lib_converted)
 
     if remove:
         _sync_delete(config, ftp, remove)
