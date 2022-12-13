f1b830e877e9cfdb5cda156e56b0e24dad3d5821
Pat <pat@p15r.net>
2022-12-13 19:54:06 +0100

    cleanup (#8)

--- fsync.py
+++ fsync.py
@@ -122,28 +122,39 @@ def _list_remote(config: Config, ftp: FTP, path: str = '',) -> Dict[str, str]:
 def _list_local(music_lib: Path) -> List[str]:
     files = []
 
-    for p in music_lib.rglob('*'):
-        # TODO: instead of string casting, use p.resolve().as_uri() &
-        #       parse target files also into URLs
-        absolute_path = p.resolve()
-        relative_path = absolute_path.relative_to(music_lib)
-        files.append(str(relative_path))
+    for path in music_lib.rglob('*'):
+        abs_path = path.resolve()
+        rel_path = abs_path.relative_to(music_lib)
+        files.append(rel_path.as_posix())
 
     return files
 
 
 def _to_list(config: Config, lib: Dict[str, str], path: str = '') -> List[str]:
-    local_list: List[str] = []
+    """
+    Converts the lib dict into a list that contains all directories and
+    files of the target system.
+    """
+
+    lib_converted: List[str] = []
 
     if path == config.target_music_lib_root_dir:
         path = ''
 
     for k, v in lib.items():
+        if len(lib) == 1 and len(v) == 0:
+            return lib_converted
+
+        # keys of value `files` store file names, but are not part of paths,
+        # hence ignored
         if k != 'files':
             if not path:
                 path = k
             else:
                 path = f'{path}/{k}'
+
+        if path:
+            lib_converted.append(path)
 
         if isinstance(v, list):
             for item in v:
@@ -153,28 +164,25 @@ def _to_list(config: Config, lib: Dict[str, str], path: str = '') -> List[str]:
                 else:
                     p = f'{path}/{item}'
 
-                local_list.append(p)
-
-        if len(lib) == 1 and len(v) == 0:
-            return local_list
+                lib_converted.append(p)
 
         if isinstance(v, dict):
-            if path != config.target_music_lib_root_dir:
-                local_list.append(path)
-            local_list.extend(_to_list(config, v, path))
+            lib_converted.extend(_to_list(config, v, path))
 
-            # we've done a directory, go back one directory
+            # all files of current directory added, go to parent directory
             path = str(Path(path).parent)
             if path == '.':
                 path = ''
 
-    return local_list
+    return lib_converted
 
 
 def _calculate_delta(local_lib: List[str], target_lib: List[str]):
     to_add = set(local_lib) - set(target_lib)
     to_delete = set(target_lib) - set(local_lib)
 
+    # sort by path length, so that files come first and
+    # can be deleted before the directories that contain the files
     add = sorted(to_add)
     delete = sorted(to_delete)
 
@@ -184,7 +192,7 @@ def _calculate_delta(local_lib: List[str], target_lib: List[str]):
         child = item.split('/')[-1]
         if len(child.split('.')) == 1:
             # TODO:
-            # if a folder has a dot in it, it would not be removed and
+            # if a folder name has a dot in it, it would not be removed and
             # thus synced to the target (solved when using Path instead of
             # strings; then we do not even add dirs to the lib.
             remove.add(item)
